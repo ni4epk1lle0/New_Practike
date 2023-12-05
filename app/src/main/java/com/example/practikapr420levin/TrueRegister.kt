0a4a6ff14141d3890d5337127167a0ca9dce89bc
Левин Вадим Валерьевич <pr120levin@oat.local>
2023-12-05 16:24:53 +0600

    update

---
+++ app/src/main/java/com/example/practikapr420levin/TrueRegister.kt
@@ -0,0 +1,11 @@
+package com.example.practikapr420levin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrueRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_true_register)
+    }
+}
